e628bd5ca2fa227093bd2570618cb0b3865e8554
soushikh <sousheiantkh@gmail.com>
2020-04-05 13:38:34 -0400

    Update max sum.cpp

--- max sum.cpp
+++ max sum.cpp
@@ -16,22 +16,22 @@ public:
 }; 
 int find_max_sum_of_nodes (BinaryTreeNode* T,  int &temp_max_sum )
 {
-    // exit if T is NULL 
+    	// exit if T is NULL 
 	if (!T)return 0; 
-    // derive the maximum sum for the left subtree 
-    int left_sum = find_max_sum_of_nodes(T->left, temp_max_sum);   
-    // derive the maximum sum for the right subtree 
-    int right_sum = find_max_sum_of_nodes(T->right, temp_max_sum);  
+	// derive the maximum sum for the left subtree 
+	int left_sum = find_max_sum_of_nodes(T->left, temp_max_sum);   
+	// derive the maximum sum for the right subtree 
+	int right_sum = find_max_sum_of_nodes(T->right, temp_max_sum);  
 		  
 	// TODO: compare T->value, left_sum + T->value, and right_sum > T-?value; store as max1
 	// TODO: compare max1, left_sum + right_sum + T->value; store as max2 
-    // TODO: update temp_max_sum with the new max 
-    // TODO: return max1 
+	// TODO: update temp_max_sum with the new max 
+	// TODO: return max1 
 }
 int find_max_sum_of_nodes(BinaryTreeNode *T)
 {
 	int temp_max_sum = INT_MIN;   
-    find_max_sum_of_nodes(T, temp_max_sum); 
-   	return temp_max_sum;
+	find_max_sum_of_nodes(T, temp_max_sum); 
+	return temp_max_sum;
 } 
     
