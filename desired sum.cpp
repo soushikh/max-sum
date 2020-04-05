571b632cfab5d1514985e047578d0b7371462e6d
soushikh <sousheiantkh@gmail.com>
2020-04-05 12:55:40 -0400

    Add files via upload

---
+++ desired sum.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class BinaryTreeNode {
+	int val;
+	BinaryTreeNode* left, * right;
+public:
+	BinaryTreeNode(int new_val) : val(new_val), left(NULL), right(NULL) {}
+	void print() { cout << val << " "; }
+
+	friend class BinaryTree;
+	friend class BinaryTreeTest;	
+};
+void find_and_print_sum_of_nodes (BinaryTreeNode* T, int desired_sum, int cur_sum, string buffer)
+{
+	// exit if T is NULL 
+	 if (!T ) return; 
+	    // update the ongoing sum with the current value for T 
+	int new_sum = cur_sum + T->value; 
+	    // update the current path string 
+	string new_buffer = buffer + " " + int2ascii(T->value); 
+	    // if the desired sum is found, print the corresponding path  
+	 if (new_sum == desired_sum) 
+	        cout << new_buffer << endl; 
+	        
+	//continue down the left subtree 
+	find_and_print_sum_of_nodes (T, desired_sum, cur_sum, buffer)
+	// TODO: continue down the right subtree 
+	// TODO: restart from the left subtree if buffer ? “” 
+	// TODO: restart from the right subtree if buffer ? “”
+}
+    
